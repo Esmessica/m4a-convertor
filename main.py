8b441032b6b9ad5d58a9c5fb91db6537ed14def0
Anna Lorencová <a.lorencov@seznam.cz>
2023-08-15 13:05:23 +0200

    convertor uses different library

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from moviepy.editor import AudioFileClip
 import os
-from pydub import AudioSegment
 
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+m4a_folder = os.path.join(script_directory, "m4a_files")
+mp3_folder = os.path.join(script_directory, "mp3_files")
+
+
+if not os.path.exists(mp3_folder):
+    os.makedirs(mp3_folder)
+
+
+m4a_files = [f for f in os.listdir(m4a_folder) if f.endswith(".m4a")]
+
+
+for m4a_file in m4a_files:
+    input_path = os.path.join(m4a_folder, m4a_file)
+    output_file = os.path.splitext(m4a_file)[0] + ".mp3"
+    output_path = os.path.join(mp3_folder, output_file)
+    audio_clip = AudioFileClip(input_path)
+    audio_clip.write_audiofile(output_path)
+
+print("Conversion complete.")
